edede3e631592d5d22c5b862602128d8835f2566
Matthew Shelley <46871802+mattscripted@users.noreply.github.com>
2020-08-18 14:14:32 -0400

    Implement search text box, submit button, and results using TDD

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App () {
+  const [searchQuery, setSearchQuery] = useState('')
+  const [hasResults, setHasResults] = useState(false)
+
+  const handleChangeSearchQuery = event => {
+    setSearchQuery(event.target.value)
+  }
+
+  const handleSubmit = () => {
+    setHasResults(true)
+  }
+
   return (
     <div>
-      This is an empty project
+      <input
+        type='text'
+        placeholder='Search'
+        // We need aria-label for getByRole()
+        aria-label='Search'
+        value={searchQuery}
+        onChange={handleChangeSearchQuery}
+      />
+      <button
+        type='submit'
+        disabled={searchQuery === ''}
+        onClick={handleSubmit}
+      >
+        Submit
+      </button>
+      {hasResults && (
+        <div data-testid='search-results'>
+          search results
+        </div>
+      )}
     </div>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,71 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
 import App from './App'
 
 describe('App', () => {
-  test('renders "This is an empty project"', () => {
-    const { getByText } = render(<App />)
-    const emptyProjectElement = getByText(/This is an empty project/i)
-    expect(emptyProjectElement).toBeInTheDocument()
+  const getSearchTextInput = () => screen.getByRole('textbox', { name: /Search/i })
+  const getSearchButton = () => screen.getByRole('button', { name: /Submit/i })
+  const querySearchResults = () => screen.queryByTestId('search-results')
+
+  it('renders a textbox', () => {
+    render(<App />)
+    expect(getSearchTextInput()).toBeInTheDocument()
+  })
+
+  it('renders a search button', () => {
+    render(<App />)
+    expect(getSearchButton()).toBeInTheDocument()
+  })
+
+  it('disables the search button, if the search textbox is empty', async () => {
+    render(<App />)
+
+    const searchTextInput = getSearchTextInput()
+    const searchButton = getSearchButton()
+
+    userEvent.clear(searchTextInput)
+
+    await waitFor(() => {
+      expect(searchButton).toBeDisabled()
+    })
+  })
+
+  it('enables the search button, if the search textbox contains text', async () => {
+    render(<App />)
+
+    const searchTextInput = getSearchTextInput()
+    const searchButton = getSearchButton()
+
+    userEvent.type(searchTextInput, 'something')
+
+    await waitFor(() => {
+      expect(searchButton).toBeEnabled()
+    })
+  })
+
+  it('displays search results after searching', async () => {
+    render(<App />)
+
+    const searchTextInput = getSearchTextInput()
+    const searchButton = getSearchButton()
+
+    userEvent.type(searchTextInput, 'something')
+    userEvent.click(searchButton)
+
+    await waitFor(() => {
+      expect(querySearchResults()).toBeInTheDocument()
+    })
+  })
+
+  // We should write this test after we fetch search results,
+  // as otherwise this element will not exist and we'll
+  // automatically pass this test
+  it('does not render search results before searching', () => {
+    render(<App />)
+
+    // We need to user query when things might not exist
+    expect(querySearchResults()).not.toBeInTheDocument()
   })
 })
