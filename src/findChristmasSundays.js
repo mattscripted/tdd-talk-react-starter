5fcb84358bc91fff24f6584923a275a93cdee78f
Matt Shelley <mattscripted@users.noreply.github.com>
2021-05-02 18:29:42 -0400

    Create base files for findChristmasSundays

---
+++ src/findChristmasSundays.js
@@ -0,0 +1,5 @@
+function findChristmasSundays() {
+
+}
+
+export default findChristmasSundays
---
+++ src/findChristmasSundays.test.js
@@ -0,0 +1,7 @@
+import findChristmasSundays from './findChristmasSundays';
+
+describe('findChristmasSundays', () => {
+  it('should do something', () => {
+    expect(true).toEqual(true);
+  })
+})
